ad5631d692968bcb5ab6600ceda757b1273e677a
Asiya Husna Vaniyambadi <111346190+husnaanjum1990@users.noreply.github.com>
2022-10-27 12:23:02 +0530

    update1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1=Math.floor(Math.random()*6+1);
-var diceImagesource="../images/dice"+randomNumber1+".png";
+var diceImagesource="dice"+randomNumber1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",diceImagesource);
 
 var randomNumber2=Math.floor(Math.random()*6+1);
-var diceImagesource2="../images/dice"+randomNumber2+".png";
+var diceImagesource2="dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",diceImagesource2);
 
 if(randomNumber1>randomNumber2){
